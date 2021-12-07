162281ba72d57aeae75699c853b983ccd6ef2caf
waranat suttikarn <waranat.cpe@gmail.com>
2021-12-08 01:26:37 +0700

    fix: remove shell script

--- php7.sh
+++
@@ -1,4 +0,0 @@
-arm brew unlink php
-arm brew link php@7.3
-echo 'export PATH="/usr/local/opt/php@7.3/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="/usr/local/opt/php@7.3/sbin:$PATH"' >> ~/.zshrc
--- php8.sh
+++
@@ -1,4 +0,0 @@
-arm brew unlink php@7.3
-arm brew link php@8.0
-echo `export PATH="/usr/local/opt/php@7.3/bin:$PATH"` >> ~/.zshrc
-echo `export PATH="/usr/local/opt/php@7.3/sbin:$PATH"` >> ~/.zshrc
